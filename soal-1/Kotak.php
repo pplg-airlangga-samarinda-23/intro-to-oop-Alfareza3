431fd752eed556cbf6859539d886346cdf239fad
Dimas_Fahri_Alfareza <dimasfaal@gmail.com>
2024-10-02 22:32:14 +0800

    Selesai

--- soal-1/Kotak.php
+++ soal-1/Kotak.php
@@ -2,28 +2,43 @@
 class PersegiPanjang
 {
     // deklarasikan panjang dan lebar
-
-
+    private $panjang;
+    private $lebar;
     // deklarasikan metode konstruktor
-
+    public function __construct($panjang, $lebar){
+        $this->panjang = $panjang;
+        $this->lebar = $lebar;
+    }
 
     // deklarasikan metode mutator dan asesor
+    public function __setPanjang($panjang){
+        $this->panjang = $panjang;
+    }
+    public function __getPanjang($panjang){
+        return $this->panjang;
+    }
+    public function __setLebar($lebar){
+        $this->lebar = $lebar;
+    }
 
-
+    public function __getLebar($lebar){
+        return $this->lebar;
+    }
 
     public function hitungLuas()
     {
         // lengkapi kode untuk menghitung luas persegi panjang
-
+        return $this->panjang * $this->lebar;
     }
 
     public function hitungKeliling()
     {
         // lengkapi dengan kode untuk menghitung keliling persegi panjang
-
+        return 2 * ($this->panjang + $this->lebar);
     }
 }
 
 $contoh = new PersegiPanjang(12, 20);
 echo "Luas Persegi Panjang: " . $contoh->hitungLuas() . "<br>";
 echo "Keliling Persegi Panjang: " . $contoh->hitungKeliling() . "<br>";
+?>
--- soal-3/Mobil.php
+++ soal-3/Mobil.php
@@ -1,15 +1,38 @@
 <?php
 class Mobil {
-    // lengkapi dengan atribut dan metode sesuai pada buku
+    public $merk;
+    public $warna;
+    public $kecepatan;
 
-    // contoh metode melaju()
-    public function melaju() {
-        echo "Mobil sedang melaju.";
+    public function __construct($merk, $warna, $kecepatan) {
+        $this->merk = $merk;
+        $this->warna = $warna;
+        $this->kecepatan = $kecepatan;
     }
-    
+
+    public function melaju() {
+        echo "Mobil " . $this->merk . " berwarna " . $this->warna . " sedang melaju dengan kecepatan " . $this->kecepatan . " km/jam.<br>";
+    }
+
+    public function berhenti() {
+        echo "Mobil " . $this->merk . " telah berhenti.<br>";
+    }
+
+    public function tambahKecepatan($tambahan) {
+        $this->kecepatan += $tambahan;
+        echo "Kecepatan mobil " . $this->merk . " bertambah menjadi " . $this->kecepatan . " km/jam.<br>";
+    }
+
+    public function kurangiKecepatan($pengurangan) {
+        $this->kecepatan -= $pengurangan;
+        echo "Kecepatan mobil " . $this->merk . " berkurang menjadi " . $this->kecepatan . " km/jam.<br>";
+    }
 }
 
-// buat sebuah objek mobil dan lengkapi atributnya
+$mobilSaya = new Mobil("Toyota", "Hitam", 60);
 
-
-// panggil keempat metode yang terdapat pada kelas Mobil
+$mobilSaya->melaju();
+$mobilSaya->tambahKecepatan(20);
+$mobilSaya->kurangiKecepatan(30);
+$mobilSaya->berhenti();
+?>
